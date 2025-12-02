127748615520ab5a0e0243b4fd7c98f30036cee2
Mitish Raina <rainamitish7@gmail.com>
2025-12-02 21:31:30 +0530

    build: training & testing completed

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from src.ds_project.logger import logging
 from src.ds_project.exception import CustomException
-from src.ds_project.components.data_ingestion import DataIngestion
-from src.ds_project.components.data_ingestion import DataIngestionConfig
+from src.ds_project.components.data_ingestion import DataIngestion, DataIngestionConfig
 from src.ds_project.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.ds_project.components.model_trainer import ModelTrainer, ModelTrainerConfig
 import sys
 
 
@@ -13,9 +13,15 @@ if __name__ == "__main__":
         # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+        
         # data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        
+        # model training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    
         
     except Exception as e:
         logging.info("this is custom exception(app.py)")
--- src/ds_project/components/data_transformation.py
+++ src/ds_project/components/data_transformation.py
@@ -63,8 +63,8 @@ class DataTransformation:
             target_feature_train_df = train_df[target_column_name]
             
             #dividing test dataset to independent and dependent features
-            input_features_test_df = train_df.drop(columns=[target_column_name], axis=1)
-            target_feature_test_df = train_df[target_column_name]
+            input_features_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
             
             logging.info("Applying preprocessing object on training and testing datasets")
             
